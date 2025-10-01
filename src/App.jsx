5b6b78c2bbeafcfafd4a1f44b1a518140341ea21
WaleedShalal <waleed.elbana2011@gmail.com>
2025-10-01 21:21:39 +0300

    first commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut, at eos
+        labore voluptatum dolorem impedit aperiam expedita nobis amet, ullam
+        distinctio consequatur maiores soluta! Non dicta asperiores cumque sed.
+      </p>
+    </div>
+  );
+}
+
+export default App;
