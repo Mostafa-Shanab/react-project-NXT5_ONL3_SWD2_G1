e86580b5a2f42abaead5c459f4429c6a0efee909
WaleedShalal <waleed.elbana2011@gmail.com>
2025-10-05 20:59:07 +0300

    adding useState, useEffect and header links

--- src/components/TheHeader/TheHeader.jsx
+++ src/components/TheHeader/TheHeader.jsx
@@ -1,13 +1,28 @@
+import { useEffect } from "react";
 import "./TheHeader.css";
 
-function TheHeader() {
+function TheHeader({ headerLinks }) {
+  console.log("Hello From TheHeader Child Component Before useEffect");
+  useEffect(() => {
+    console.log("Hello From TheHeader Child Component From useEffect 1");
+    // This function will be fired after component unmount or deleted mean removed from DOM
+    return () =>
+      console.log(
+        "Hello From TheHeader Child Component From useEffect 1 After UnMountttttttttttttttttttttt"
+      );
+  }, []);
+
+  console.log("Hello From TheHeader Child Component After useEffect");
+
   return (
     <header>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
+          {headerLinks.map(({ title, path }) => (
+            <li key={path}>
+              <a href={path}>{title}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
